dc2c30760257784c010820ac286cc1c399d96d53
Ermolaev Andrey <andruhafirst@yandex.ru>
2016-02-17 20:45:24 +0300

    blueprint

--- simple_ambari/site-cookbooks/ambari/recipes/blueprints.rb
+++ simple_ambari/site-cookbooks/ambari/recipes/blueprints.rb
@@ -2,20 +2,20 @@ ambari_server_fqdn = node['ambari']['server_fqdn']
 
 blueprint_name = "ermolaev_blueprint"
 cluster_name   = "ermolaev_cluster"
-basic_auth_parameters = "#{node['ambari']['admin_user']}:#{node['ambari']['admin_password']}"
+basic_auth_parameters = Base64.encode64("#{node['ambari']['admin_user']}:#{node['ambari']['admin_password']}")
 
 
 http_request 'Init Blueprints' do
-  url      "#{ambari_server_fqdn}:8080/api/v1/blueprints/#{blueprint_name}"
+  url      "http://#{ambari_server_fqdn}:8080/api/v1/blueprints/#{blueprint_name}"
   action   :post
-  headers  { "AUTHORIZATION" => "Basic #{basic_auth_parameters}", "X-Requested-By" => "ambari-cookbook" })
+  headers  ({ "AUTHORIZATION" => "Basic #{basic_auth_parameters}", "X-Requested-By" => "ambari-cookbook" })
   message  File.read(node['ambari']['blueprints']['blueprint_json_file'])
 end
 
 
 http_request 'Init Cluster' do
-  url      "#{ambari_server_fqdn}:8080/api/v1/clusters/#{cluster_name}"
+  url      "http://#{ambari_server_fqdn}:8080/api/v1/clusters/#{cluster_name}"
   action   :post
-  headers  { "AUTHORIZATION" => "Basic #{basic_auth_parameters}", "X-Requested-By" => "ambari-cookbook" })
+  headers  ({ "AUTHORIZATION" => "Basic #{basic_auth_parameters}", "X-Requested-By" => "ambari-cookbook" })
   message  File.read(node['ambari']['blueprints']['cluster_json_file'])
 end
